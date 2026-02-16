ffab16dc545ee0a1affad17613679672f0281559
ChrisVN123 <christianvanell@icloud.com>
2026-02-16 10:19:30 +0100

    fixed

--- a1/cvn/ex3_2.py
+++ a1/cvn/ex3_2.py
@@ -19,6 +19,47 @@ def fdcoeffV(k, xbar, x):
     c = np.linalg.solve(A, b)
     return c 
 
+
+def fdcoeffF(k: int, xbar, x):
+    # k'th derivative of u is being approximated
+    # derivative evaluated at xbar
+    # x are grid points
+    # length(x) > k
+    n = len(x)
+    x = np.asarray(x, dtype=float)
+    if k >= n:
+        raise Exception("len(x) must be greater than k")
+
+    m = k
+
+    c1 = 1.0  # becomes product of distances between stencil pts from prev. steps
+    c4 = x[0] - xbar  # offset from xbar
+    C = np.zeros((n, m + 1), dtype=float)  # row for each point, col for each derivative
+    C[0, 0] = 1.0  # if only one point then u(xbar) = u(xbar)
+
+    for i in range(1, n):  # add stencil point one at a time
+        mn = min(i, m)  # only i stencil points available, so max order of deriv
+        c2 = 1.0  # used to calc distances from new point to all older points
+        c5 = c4  # old point's offset
+        c4 = x[i] - xbar  # new point's offset
+
+        for j in range(0, i):  # loop over old points
+            c3 = x[i] - x[j]  # dist new point to each old point
+            c2 = c2 * c3  # all distances multiplied
+            if j == i - 1:
+                for s in range(mn, 0, -1):  # create stencil weights for x(i1)
+                    # stencil weights for s1 w. derivs less than max order of deriv
+                    C[i, s] = c1 * (s * C[i - 1, s - 1] - c5 * C[i - 1, s]) / c2
+
+                C[i, 0] = -c1 * c5 * C[i - 1, 0] / c2
+
+            for s in range(mn, 0, -1):
+                C[j, s] = (c4 * C[j, s] - s * C[j, s - 1]) / c3
+            C[j, 0] = (c4 * C[j, 0]) / c3
+        c1 = c2
+    c = C[:, k]
+    return c
+
 def u(x):
     return np.exp(np.cos(x))
 
@@ -36,19 +77,26 @@ print("Exact u''(0) =", exact)
 
 num = 30
 err = np.zeros(num-1)
+err_centered = np.zeros(num-1)
 hvals = np.zeros(num-1)
 
 i = 0
 for m in [ i for i in range(2, num)]:
     h = 1 / m**2
-    print(m**2)
     hvals[i] = h
 
     x_nodes = xbar + h * np.arange(0, 5)
+    x_nodes2 = xbar + h * np.arange(-2,3)
 
-    c = fdcoeffV(k, xbar, x_nodes)
+
+    c = fdcoeffF(k, xbar, x_nodes)
+    c_centered = fdcoeffF(k, xbar, x_nodes2)
+
     approx = np.dot(c, u(x_nodes))
+    approx_centered = np.dot(c_centered, u(x_nodes2))
+
     err[i] = abs(approx - exact)
+    err_centered[i] = abs(approx_centered - exact)
 
     i += 1
 
@@ -59,13 +107,17 @@ print(f"h={h: .3e}   approx={approx: .12f}   err={err[-1]: .3e}")
 mask = np.isfinite(err) & (err > 0) & np.isfinite(hvals) & (hvals > 0)
 
 p = np.polyfit(np.log(hvals[mask]), np.log(err[mask]), 1)[0]
-print("Estimated convergence rate p =", p)
+p_cent = np.polyfit(np.log(hvals[mask]), np.log(err_centered[mask]), 1)[0]
+print("Estimated convergence rate p_non_centered =", p)
+print("Estimated convergence rate p_centered =", p_cent)
 
-plt.loglog(hvals, err, 'o-')
+plt.loglog(hvals, err, 'o-', label = 'not centered')
+plt.loglog(hvals, err_centered, 'o-',label = 'centered')
 plt.xlabel("h")
 plt.ylabel("error")
+plt.legend()
 plt.title("Convergence of errors with f(x) = exp(cos(x))")
-plt.savefig("ex3_error.png")
+plt.savefig("img/ex3_error.png")
 
 plt.plot(approx)
-plt.savefig("ex3_approx_func.png")
+plt.savefig("img/ex3_approx_func.png")
